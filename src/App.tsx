d4c3c60ae881fa583fc17377aaeb1a541ef345dd
oratake <smith.shimomura@gmail.com>
2023-05-26 01:46:05 +0900

    temp: 230525

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+
+interface SpeakerList {
+  id: number;
+  speakerName: string;
+  title: string;
+}
+
+const ltList: SpeakerList[] = [
+  {
+    id: 1,
+    speakerName: "oratake",
+    title: "LTどうでしょう",
+  },
+  {
+    id: 2,
+    speakerName: "よしはる",
+    title: "タイトル",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [speakerList, setSpeakerList] = useState(ltList as SpeakerList[]);
+  const [isShuffle, setIsShuffle] = useState(false);
+
+  useEffect(() => {
+    if (isShuffle) {
+      setInterval(() => {
+        const randomIndex = Math.floor(Math.random() * speakerList.length);
+        const temp = speakerList[randomIndex];
+        speakerList[randomIndex] = speakerList[0];
+        speakerList[0] = temp;
+      }, 100);
+    }
+  }, [isShuffle]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => setIsShuffle(!isShuffle)}>
+        {isShuffle ? "Stop" : "Start"}
+      </button>
+      {speakerList.map((element, index) => (
+        <div key={index}>{element.speakerName}{element.speakerName !== "oratake" ? "さん" : ""}: {element.title}</div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
